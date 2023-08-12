79cfbc733c1e2a22a06a17bc3750cc3a8cbe5318
oghene-ella <oghenekaro57@gmail.com>
2023-08-12 15:46:17 +0100

    worked on the age validation

--- js/index.js
+++ js/index.js
@@ -9,46 +9,154 @@ const dayResult = document.querySelector(".num-days");
 const monthResult = document.querySelector(".num-months");
 const yearResult = document.querySelector(".num-years");
 
+const labYear = document.querySelector(".lab-year");
+const labMonth = document.querySelector(".lab-month");
+const labDay = document.querySelector(".lab-day");
+
+const errorYear = document.querySelector(".error-year");
+const errorMonth = document.querySelector(".error-month");
+const errorDay = document.querySelector(".error-day");
+
+
 //use the date function
 const the_date = new Date();
 const current_year = the_date.getFullYear();
 const current_day = the_date.getDate();
 const current_month = the_date.getMonth() + 1 ;
-console.log(current_month);
 
+
+// day input code
 const dayValue = (e) => {
-    console.log(e.target.value);
+  const dayVal = e.target.value;
+
+  if (dayVal > 0 && dayVal <= 31) {
     dayResult.textContent = e.target.value;
-    console.log(current_day);
+    labDay.style.color = "#008000";
+    dayInput.style.border = "1px solid #008000";
+    errorDay.textContent = "Good";
+    errorDay.style.color = "#008000";
+  } 
+  else {
+    dayResult.textContent = "- -";
+    labDay.style.color = "#FF0000";
+    dayInput.style.border = "1px solid #FF0000";
+    errorDay.textContent = "Invalid Day";
+    errorDay.style.color = "#FF0000";
+    errorDay.style.fontSize = "8px";
+  }
 }
 
+// add focus event to the day input
+dayInput.addEventListener("focus", function () {
+  errorDay.textContent = "";
+  dayInput.style.border = "1px solid blue";
+});
+
+// add blur event for the day input
+dayInput.addEventListener("blur", function () {
+  if (!dayInput.value) {
+    labDay.style.color = "#FF0000";
+    errorDay.textContent = "Day is required!";
+    dayInput.style.border = "1px solid #FF0000";
+    errorDay.style.color = "#FF0000";
+    errorDay.style.fontSize = "8px";
+  } 
+});
+
+
+// month input code
 const monthValue = (e) => {
   console.log(e.target.value);
   const getMonth = e.target.value 
-  if( getMonth > 12) {
-    monthResult.textContent = '- -'
-    //add validation error   use calender month, if is not a number , should be a number format 
-  } else {
+
+  if (getMonth > 0 && getMonth <= 12 ) {
     monthResult.textContent = e.target.value;
+    labMonth.style.color = "#008000";
+    monthInput.style.border = "1px solid #008000";
+    errorMonth.textContent = "Good";
+    errorMonth.style.color = "#008000";
+  } 
+  else {
+    monthResult.textContent = "- -";
+    labMonth.style.color = "#FF0000";
+    monthInput.style.border = "1px solid #FF0000";
+    errorMonth.textContent = "Invalid Month";
+    errorMonth.style.color = "#FF0000";
+    errorMonth.style.fontSize = "8px";
   }
- 
-
 };
 
+// add focus to the month input
+monthInput.addEventListener("focus", function () {
+  errorMonth.textContent = "";
+  monthInput.style.border = "1px solid blue";
+  monthResult.textContent = "- -";
+  // labMonth.style.color = "green";
+});
+
+// add blur event for the month input
+monthInput.addEventListener("blur", function () {
+  if (!monthInput.value) {
+    labMonth.style.color = "#FF0000";
+    errorMonth.textContent = "month is required!";
+    monthInput.style.border = "1px solid #FF0000";
+    errorMonth.style.color = "#FF0000";
+    errorMonth.style.fontSize = "8px";
+    monthResult.textContent = "- -"
+  }
+});
+
+
+// year input code
 const yearValue = (e) => {
-  console.log(e.target.value);
-  yearResult.textContent = current_year - e.target.value;
-/*
-//   if (e.target.value <= 1930 || e.target.value >= current_year){
-//     document.querySelector(".year").style.color = "#FF0000";
-//     yearInput.style.border = "2px solid #FF0000";
-//     console.log("you don dey do too much!")
-//   }
-//   document.querySelector(".year").style.color = "#008000";
-//   yearInput.style.border = "2px solid #008000";
-*/
+  const yearVal = e.target.value;
+  if (yearVal.length === 4 && yearVal <= current_year) {
+    if (yearVal < 1930) {
+      yearResult.textContent = "- -";
+      labYear.style.color = "#FF0000";
+      yearInput.style.border = "1px solid #FF0000";
+      errorYear.textContent = "Invalid Year";
+      errorYear.style.color = "#FF0000";
+      errorYear.style.fontSize = "8px";
+    } 
+    
+    else {
+      yearResult.textContent = current_year - e.target.value;
+      labYear.style.color = "#008000";
+      yearInput.style.border = "1px solid #008000";
+      errorYear.textContent = "Good";
+      errorYear.style.color = "#008000";
+    }
+  }
 
+  else {
+    yearResult.textContent = "- -";
+    labYear.style.color = "#FF0000";
+    yearInput.style.border = "1px solid #FF0000";
+    errorYear.textContent = "Invalid Year";
+    errorYear.style.color = "#FF0000";
+    errorYear.style.fontSize = "8px";
+  }
 };
+
+// add focus event to the year input
+yearInput.addEventListener("focus", function () {
+  errorYear.textContent = "";
+  yearInput.style.border = "1px solid blue";
+});
+
+// add blur event for the year input
+yearInput.addEventListener("blur", function () {
+  if (!yearInput.value) {
+    labYear.style.color = "#FF0000";
+    errorYear.textContent = "Year is required!";
+    yearInput.style.border = "1px solid #FF0000";
+    errorYear.style.color = "#FF0000";
+    errorYear.style.fontSize = "8px";
+  } 
+});
+
+
 
 // add event listeners for all the inputs
 dayInput.addEventListener("input", dayValue);
